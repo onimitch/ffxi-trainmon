ad45c0e444d687911dcef15fe914d342fcaf3c33
David Mitchell <mitch@two-tails.com>
2023-12-22 14:06:15 +0000

    Fixed an issue where training target kills weren't detected in some situations. Fixed an issue where training data didn't repeat correctly inline with the game state.

--- chat_modes.lua
+++ chat_modes.lua
@@ -2,6 +2,7 @@ local chat_modes = T{
     player = 36, -- Player defeats the Tiny Mandragora.
     others = 37, -- Tenzen defeats the Beach Bunny.
     battle = 122, -- You defeated a designated target. (Progress: 1/4)
+    timed_battle = 142,
     unknown = 148,
     message = 150,
     system = 151,
--- monitor.lua
+++ monitor.lua
@@ -28,6 +28,25 @@ local function player_is_in_party(player_name)
     return false
 end
 
+local function array_remove(t, fn_remove)
+    local j, n = 1, #t
+
+    for i = 1, n do
+        if fn_remove(t, i, j) then
+            t[i] = nil
+        else
+            -- Move i's kept value to j's position, if it's not already there.
+            if i ~= j then
+                t[j] = t[i]
+                t[i] = nil
+            end
+            j = j + 1 -- Increment position of where we'll place the next kept value.
+        end
+    end
+
+    return t
+end
+
 function monitor:new(lang_code, print_func, encoding_module, storage_key)
     local instance = {}
     setmetatable(instance, self)
@@ -35,11 +54,13 @@ function monitor:new(lang_code, print_func, encoding_module, storage_key)
     instance.print = print_func or print
     instance.encoding = encoding_module or gdi_encoding
     instance._storage_key = storage_key or 'training_data'
-    instance._lang_code = lang_code
+    instance._lang_code = lang_code or 'en'
     instance._rules = test_rules[lang_code]
     instance._data = {}
 
     instance:reset_training_data()
+
+    LogManager:Log(5, 'Trainmon', 'monitor:new ' .. instance._lang_code .. ', ' .. instance._storage_key)
 
     -- Register for settings changes
     settings.register(instance._storage_key, instance._storage_key .. '_update', function (s)
@@ -58,6 +79,7 @@ function monitor:reset_training_data()
     self._target_monsters_options = {}
     self._target_zone_id = -1
     self._target_monster_kills = {}
+    self._monster_kills = {}
     self._waiting_confirm_end = false
     self._target_monsters_repeat = {}
     self._target_zone_id_repeat = -1
@@ -66,18 +88,62 @@ end
 function monitor:on_training_data_updated()
     self._target_monsters = self._data.target_monsters
     self._target_zone_id = tonumber(self._data.target_zone_id)
+    LogManager:Log(5, 'Trainmon', 'on_training_data_updated: _target_zone_id=' .. tostring(self._target_zone_id))
 end
 
-function monitor:save_train_data()
+function monitor:save_training_data()
     self._data.target_monsters = self._target_monsters
     self._data.target_zone_id = self._target_zone_id
+    LogManager:Log(5, 'Trainmon', 'save_training_data: _target_zone_id=' .. tostring(self._target_zone_id))
     settings.save(self._storage_key)
 end
 
-local function codes(str)
-	return (str:gsub('.', function (c)
-		return string.format('[%02X]', string.byte(c))
-	end))
+function monitor:reconcile_target_kills()
+    if #self._target_monster_kills == 0 or #self._monster_kills == 0 then
+        return
+    end
+
+    local target_kills_count = #self._target_monster_kills
+    local kills_count = #self._monster_kills
+
+    local stale = os.clock() - 5 -- seconds
+    -- Remove stale entries
+    array_remove(self._target_monster_kills, function(t, i) return t[i].time < stale end)
+    array_remove(self._monster_kills, function(t, i) return t[i].time < stale end)
+
+    if target_kills_count ~= #self._target_monster_kills or kills_count ~= #self._monster_kills then
+        LogManager:Log(5, 'Trainmon', 'removed stale kill entries: ' .. target_kills_count - #self._target_monster_kills .. ', ' .. kills_count - #self._monster_kills)
+    end
+
+    local remove_target_entries = {}
+
+    for i, target_kill in ipairs(self._target_monster_kills) do
+        local expected_count = target_kill.count - 1
+
+        local remove_kill_entries = {}
+
+        for j, monster_kill in ipairs(self._monster_kills) do
+            local target_monster = self._target_monsters[monster_kill.index]
+
+            if target_monster.count == expected_count and target_monster.total == target_kill.total then
+                target_monster.count = target_kill.count
+                LogManager:Log(5, 'Trainmon', 'reconciled kill: ' .. target_monster.name .. ' = ' .. target_monster.count .. '/' .. target_monster.total)
+                self:save_training_data()
+
+                table.insert(remove_target_entries, i)
+                table.insert(remove_kill_entries, j)
+                break
+            end
+        end
+
+        for _, j in ipairs(remove_kill_entries) do
+            table.remove(self._monster_kills, j)
+        end
+    end
+
+    for _, i in ipairs(remove_target_entries) do
+        table.remove(self._target_monster_kills, i)
+    end
 end
 
 function monitor:process_input(mode, input)
@@ -94,7 +160,7 @@ function monitor:process_input(mode, input)
             if #captures > 0 then
                 local capture_data = self._rules.confirmed_captures(captures)
                 local is_family = string.find(capture_data.name, self._rules.monster_family) ~= nil
-                -- self.print(chat.header(addon.name):append(chat.message(string.format('confirmed: %d, %s (%d/%d)', monster_index, capture_data.name, capture_data.count, capture_data.total))))
+                LogManager:Log(5, 'Trainmon', string.format('confirmed: %d, %s (%d/%d)', monster_index, capture_data.name, capture_data.count, capture_data.total))
 
                 self._target_monsters_options[tonumber(monster_index)] = {
                     name = capture_data.name,
@@ -116,8 +182,7 @@ function monitor:process_input(mode, input)
         local zone_id = AshitaCore:GetResourceManager():GetString('zones.names', self.encoding:UTF8_To_ShiftJIS(zone_name, true))
         if zone_id ~= -1 then
             self._target_zone_id = zone_id
-            -- self.print(chat.header(addon.name):append(chat.message(string.format('Updated training data for: %s', zone_name))))
-            self:save_train_data()
+            self:save_training_data()
         else
             self.print(chat.header(addon.name):append(chat.error(string.format('Unknown training zone: "%s"', zone_name))))
             local player_zone_id = tonumber(AshitaCore:GetMemoryManager():GetParty():GetMemberZone(0))
@@ -134,7 +199,7 @@ function monitor:process_input(mode, input)
             -- Use the last options we parsed
             self._target_monsters = self._target_monsters_options
             self._target_monsters_options = {}
-            self:save_train_data()
+            self:save_training_data()
         end
         return
     end
@@ -150,7 +215,7 @@ function monitor:process_input(mode, input)
             if #captures > 0 then
                 local capture_data = self._rules.options_captures(captures)
                 local is_family = string.find(capture_data.name, self._rules.monster_family, 1, true) ~= nil
-                -- self.print(chat.header(addon.name):append(chat.message(string.format('options: %d, %s (%d)', monster_index, capture_data.name, capture_data.total))))
+                LogManager:Log(5, 'Trainmon', string.format('options: %d, %s (%d)', monster_index, capture_data.name, capture_data.total))
 
                 self._target_monsters_options[tonumber(monster_index)] = {
                     name = capture_data.name,
@@ -165,46 +230,46 @@ function monitor:process_input(mode, input)
     end
 
     -- User accepted the last viewed training options
-    if mode == chat_modes.unknown and string.find(input, self._rules.accepted) then
+    if (mode == chat_modes.timed_battle or mode == chat_modes.unknown) and string.find(input, self._rules.accepted) then
         if #self._target_monsters_options > 0 then
             -- Use the last options we parsed
             self._target_monsters = self._target_monsters_options
             self._target_monsters_options = {}
             self._target_zone_id = tonumber(AshitaCore:GetMemoryManager():GetParty():GetMemberZone(0))
-            self:save_train_data()
+            self:save_training_data()
         end
         return
     end
 
     -- Training was cancelled
-    if mode == chat_modes.unknown and string.find(input, self._rules.cancelled) then
+    if (mode == chat_modes.timed_battle or mode == chat_modes.unknown) and string.find(input, self._rules.cancelled) then
         self:reset_training_data()
-        self:save_train_data()
+        self:save_training_data()
         return
     end
 
     -- Active Training
     if #self._target_monsters > 0 then
-        -- A target monster was killed. We don't know what the monster is yet, so make a note of the totals
+        -- A target monster was killed which gives us the kill count, but not the name of the monster.
         if mode == chat_modes.battle and string.find(input, self._rules.target_monster_killed) then
             local count, total = string.match(input, self._rules.target_monster_killed)
-            table.insert(self._target_monster_kills, ({ count = tonumber(count), total = tonumber(total) }))
+            table.insert(self._target_monster_kills, ({ count = tonumber(count), total = tonumber(total), time = os.clock() }))
+
+            self:reconcile_target_kills()
             return
         end
 
-        -- Monster killed, if this happens after a target_monster_killed message then we link them together
-        if #self._target_monster_kills > 0 and (mode == chat_modes.player or mode == chat_modes.others) then
+        -- Monster killed but we don't know if it's part of training or not
+        if mode == chat_modes.player or mode == chat_modes.others then
             local killed_by, monster_name = string.match(input, self._rules.monster_killed_by)
-
+            -- Make sure who killed the monster is in the party (includes the player)
             if monster_name ~= nil and player_is_in_party(killed_by) then
-                -- Monster has now been killed, so get the monster name and link it to the last count update
-                local last_target_kill = self._target_monster_kills[1]
-                local total = last_target_kill.total
-                local new_count = last_target_kill.count
-                local expected_count = new_count - 1
+                LogManager:Log(5, 'Trainmon', 'Monster killed: ' .. monster_name .. ', by ' .. killed_by)
 
                 -- Find the target monster in our list
                 local monster_index = 0
+
+                -- Name match
                 for i, v in ipairs(self._target_monsters) do
                     if v.name == monster_name then
                         monster_index = i
@@ -239,46 +304,10 @@ function monitor:process_input(mode, input)
                     end
                 end
 
-                -- Check the counts match with what we're expecting
                 if monster_index > 0 then
-                    local target_monster = self._target_monsters[monster_index]
-                    if target_monster.count ~= expected_count then
-                        -- Expected count doesn't match, which means the data has got out of sync
-                        -- If we only have this single target kill recorded, it should be safe to use this
-                        -- If we have more than one target kill pending, then we can't be sure this is a match
-                        if #self._target_monster_kills > 1 then
-                            self.print(chat.header(addon.name):append(chat.error('Error: Found "%s" in training data but data is out of sync (count=%d, expected=%d).\r\nPlease reconfirm training data at a Field Manual.')):fmt(monster_name, target_monster.count, expected_count))
-                            self:reset_training_data()
-                            self:save_train_data()
-                            return
-                        end
-                    end
+                    table.insert(self._monster_kills, ({ name = monster_name, index = monster_index, time = os.clock() }))
+                    self:reconcile_target_kills()
                 end
-
-                -- If we couldn't match by family name, then fall back to match by total & expected_count on a family
-                -- if monster_index == 0 then
-                --     for i, v in ipairs(self._target_monsters) do
-                --         if v.is_family and v.total == total and v.count == expected_count then
-                --             monster_index = i
-                --             break
-                --         end
-                --     end
-                --     -- We did find a match, but it might not be correct
-                --     if monster_index ~= 0 then
-                --         -- TODO: Print a warning?
-                --     end
-                -- end
-
-                if monster_index == 0 then
-                    -- Monster not found, ignore since it's not part of our training
-                    self.print(chat.header(addon.name):append(chat.error('Failed to find "%s" in Training Data (%d/%d)')):fmt(monster_name, new_count, total))
-                    return
-                end
-
-                self._target_monsters[monster_index].count = new_count
-                table.remove(self._target_monster_kills, 1)
-                self:save_train_data()
-                return
             end
         end
 
@@ -290,10 +319,12 @@ function monitor:process_input(mode, input)
             end
 
             -- Make a copy of the target_monsters incase we repeat the training
-            self._target_monsters_repeat = self._target_monsters
-            self._target_zone_id_repeat = self._target_zone_id
+            local target_monsters_repeat = self._target_monsters
+            local target_zone_id_repeat = self._target_zone_id
             self:reset_training_data()
-            self:save_train_data()
+            self:save_training_data()
+            self._target_monsters_repeat = target_monsters_repeat
+            self._target_zone_id_repeat = target_zone_id_repeat
             return
         end
     end
@@ -304,7 +335,7 @@ function monitor:process_input(mode, input)
         self._target_monsters_repeat = {}
         self._target_zone_id = self._target_zone_id_repeat
         self._target_zone_id_repeat = -1
-        self:save_train_data()
+        self:save_training_data()
         return
     end
 end
